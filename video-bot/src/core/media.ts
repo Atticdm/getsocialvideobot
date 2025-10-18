d13abe2893e5b8e01d00d6d2c60836a43f709a75
Dmitrii <attic.dm@gmail.com>
2025-10-19 00:44:54 +0400

    Further boost voice levels in audio mix

--- video-bot/src/core/media.ts
+++ video-bot/src/core/media.ts
@@ -74,7 +74,7 @@ export async function mixVoiceWithInstrumental(
     voiceTrackPath,
     '-filter_complex',
     '[0:a]aresample=async=1:first_pts=0,aformat=sample_fmts=fltp:channel_layouts=stereo,volume=0.9[instr];'
-      + '[1:a]aresample=async=1:first_pts=0,aformat=sample_fmts=fltp:channel_layouts=stereo,volume=3.0[voice];'
+      + '[1:a]aresample=async=1:first_pts=0,aformat=sample_fmts=fltp:channel_layouts=stereo,volume=9.0[voice];'
       + '[instr][voice]amix=inputs=2:normalize=0:dropout_transition=0[aout]',
     '-map',
     '[aout]',
@@ -120,7 +120,7 @@ export async function mixVoiceWithBackground(
     voiceTrackPath,
     '-filter_complex',
     '[0:a]stereotools=mlev=0[bg];'
-    + '[1:a]aresample=async=1:first_pts=0,aformat=sample_fmts=fltp:channel_layouts=stereo,volume=3.0[voice];'
+    + '[1:a]aresample=async=1:first_pts=0,aformat=sample_fmts=fltp:channel_layouts=stereo,volume=9.0[voice];'
     + '[bg][voice]amix=inputs=2:normalize=0[aout]',
     '-map',
     '[aout]',
@@ -150,7 +150,7 @@ export async function mixVoiceWithBackground(
     voiceTrackPath,
     '-filter_complex',
     '[0:a]aresample=async=1:first_pts=0,aformat=sample_fmts=fltp:channel_layouts=stereo,volume=0.35[minbg];'
-    + '[1:a]aresample=async=1:first_pts=0,aformat=sample_fmts=fltp:channel_layouts=stereo,volume=3.0[voice];'
+    + '[1:a]aresample=async=1:first_pts=0,aformat=sample_fmts=fltp:channel_layouts=stereo,volume=9.0[voice];'
     + '[minbg][voice]amix=inputs=2:normalize=0:dropout_transition=0[aout]',
     '-map',
     '[aout]',
